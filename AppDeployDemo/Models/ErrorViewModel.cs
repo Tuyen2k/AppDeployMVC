021a8c19e9f3334001d1ee380ea53eb872f820de
tuyen nguyen <tuyen4835@gmail.com>
2026-02-10 21:24:19 +0700

    init

---
+++ AppDeployDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppDeployDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
